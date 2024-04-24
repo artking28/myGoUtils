194d6082ce9bfb93ea5ba2b48a0670cc742678e1
a.andrade <arthur10andrade@gmail.com.br>
2024-04-24 11:17:39 -0300

    .

--- utils.go
+++ utils.go
@@ -32,8 +32,8 @@ type double interface {
 	float32 | float64
 }
 
-// TernaryOperator simulate the ternary operator just like (condition ? true : false)
-func TernaryOpeator[Any Atom, Any2 Atom](condition bool, ret1 Any, ret2 Any2) any {
+// TernaryOperator simulates the ternary operator just like (condition ? true : false)
+func TernaryOperator[Any Atom, Any2 Atom](condition bool, ret1 Any, ret2 Any2) any {
 	if condition {
 		return ret1
 	}
@@ -62,7 +62,7 @@ func GetTagValue[Any any](fieldName, tagName string) (string, error) {
 	return ret, nil
 }
 
-// Split by regex withot losing the delimiters
+// SplitAfterRegex splits by regex withot losing the delimiters
 func SplitAfterRegex(rgx *regexp.Regexp, str string) (ret []string) {
 	var l []int
 	i, all := 0, rgx.FindAllStringIndex(str, -1)
@@ -87,7 +87,7 @@ func SplitAfterRegex(rgx *regexp.Regexp, str string) (ret []string) {
 	return ret
 }
 
-// Get the system language
+// GetLocale get the system language
 func GetLocale() string {
 
 	var cmd *exec.Cmd
@@ -128,7 +128,7 @@ func AbsoluteFlatMap(list []interface{}) []interface{} {
 	return ret
 }
 
-// GetFilesInto create an array composed by every file into a folder, you can filter by file type
+// GetFilesInto creates an array composed by every file into a folder, you can filter by file type
 func GetFilesInto(path string, extentionFilter string) ([]os.FileInfo, error) {
 	var ret []os.FileInfo
 	return ret, filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
@@ -142,7 +142,7 @@ func GetFilesInto(path string, extentionFilter string) ([]os.FileInfo, error) {
 	})
 }
 
-// Int convert any valid numeral string into number
+// Int converts any valid numeral string into number
 func Int[intType number](str string) (intType, error) {
 	ret, err := strconv.ParseInt(strings.TrimSpace(str), 10, 64)
 	if err != nil {
@@ -151,7 +151,7 @@ func Int[intType number](str string) (intType, error) {
 	return intType(ret), nil
 }
 
-// Float convert any valid floating point string to a real floating point
+// Float converts any valid floating point string to a real floating point
 func Float[floatType double](str string) (floatType, error) {
 	ret, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
 	if err != nil {
@@ -160,7 +160,7 @@ func Float[floatType double](str string) (floatType, error) {
 	return floatType(ret), nil
 }
 
-// String convert anything to `string`
+// String converts anything to `string`
 func String[Any any](some Any) string {
 	return fmt.Sprintf("%v", some)
 }
@@ -170,11 +170,7 @@ func Ptr[Any any](some Any) *Any {
 	return &some
 }
 
-/*
-	PointerVal returns the value held by a pointer. If the pointer
-
-is nil the zerovalue is returned.
-*/
+// PtrVal returns the value held by a pointer. If the pointer is nil, a zero value is returned.
 func PtrVal[Any any](some *Any) Any {
 	if some == nil {
 		var ret Any
